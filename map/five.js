45c48a9354c9857672e5989d5af190944b0c7a28
abhishek <abhishekparayil01@gmail.com>
2024-11-02 09:40:29 +0530

    mapping

---
+++ map/five.js
@@ -0,0 +1,8 @@
+const users=[
+    {firstName:"john",lastName:"Biden",age:26},
+    {firstName:"jimmy",lastName:"cob",age:75},
+    {firstName:"sam",lastName:"lewis",age:50},
+    {firstName:"Ronald",lastName:"Mathew",age:26},
+  ];
+  let a= users.map(b=>b.firstName +" "+ b.lastName)
+  console.log(a)
